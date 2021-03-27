b02b411deb6a6d2a1a79c506d8ce8217268d96a9
spezi77 <spezi7713@gmx.net>
2021-03-27 15:14:27 +0100

    SystemUI: Also show the battery bar if the battery is not charging. * Only show it while in doze mode (Ambient/AoD)
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -744,7 +744,7 @@ public class KeyguardIndicationController {
                         mTopIndicationView.switchIndication(indication, null, false /* animate */,
                                 null /* onAnimationEndCallback */);
                     }
-                    if (showBatteryBar) {
+                    if (showBatteryBar || showBatteryBarAlways) {
                         mBatteryBar.setVisibility(View.VISIBLE);
                         mBatteryBar.setBatteryPercent(mBatteryLevel);
                         mBatteryBar.setBarColor(Color.WHITE);
@@ -754,6 +754,11 @@ public class KeyguardIndicationController {
                             .format(mBatteryLevel / 100f);
                     mTopIndicationView.switchIndication(percentage, null /* indication */,
                             false /* animate */, null /* onAnimationEnd*/);
+                    if (showBatteryBarAlways) {
+                        mBatteryBar.setVisibility(View.VISIBLE);
+                        mBatteryBar.setBatteryPercent(mBatteryLevel);
+                        mBatteryBar.setBarColor(Color.WHITE);
+                    }
                 }
                 return;
             }
