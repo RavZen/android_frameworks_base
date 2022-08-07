5e295c56438d8252687d034f2c5f7b9dce184690
Viktor Hermann <v.hermann.it@gmail.com>
2022-08-08 00:05:22 +0200

    [SQUASH] Custom Clock improvments
    DefaulClock: Move up from fp a bit
    
    Keyguard: Improve custom clock position
    OOS-Clock: Improve big clock view
    
    
    CustomClock: Improvements

--- packages/SystemUI/src/com/android/keyguard/clock/OOSClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/OOSClockController.java
@@ -121,7 +121,7 @@ public class OOSClockController implements ClockPlugin {
 
 
     public int getPreferredY(int totalHeight) {
-        return totalHeight / 2;
+        return totalHeight;
     }
 
 
