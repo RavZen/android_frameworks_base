fbb1e229b01e3d72186c899d9340d9f23a6a9efa
Viktor Hermann <v.hermann.it@gmail.com>
2022-08-08 10:04:49 +0200

    SystemUI: reTicker [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5355,6 +5355,18 @@ public final class Settings {
         public static final String ENABLE_TASKBAR = "enable_taskbar";
 
         /**
+         * reTicker Status
+         * @hide
+         */
+        public static final String RETICKER_STATUS = "reticker_status";
+
+        /**
+         * reTicker Colored
+         * @hide
+         */
+        public static final String RETICKER_COLORED = "reticker_colored";
+
+        /**
          * These are all public system settings
          *
          * @hide
