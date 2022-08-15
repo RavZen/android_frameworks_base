8138b50fecb78e8298100896c0097bed989c5e3a
Viktor Hermann <v.hermann.it@gmail.com>
2022-08-15 16:13:47 +0200

    test

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -33,9 +33,11 @@ public class PixelPropsUtils {
 
     private static volatile boolean sIsGms = false;
     public static final String PACKAGE_GMS = "com.google.android.gms";
-    public static final String PACKAGE_ANDROID = "com.google.android";
-    public static final String PACKAGE_LATIN = "com.google.android.inputmethod.latin";
     private static final String DEVICE = "ro.statix.device";
+
+    private static final Map<String, Object> propsToChangePixel6;
+    private static final String[] packagesToChangePixel6 = { PACKAGE_GMS,
+            "com.google.android", "com.samsung.accessory", "com.samsung.android", "com.android.vending", "com.google.android.inputmethod.latin", };
 
     private static final Map<String, Object> propsToChange;
     private static final Map<String, ArrayList<String>> propsToKeep;
@@ -65,10 +67,18 @@ public class PixelPropsUtils {
     };
 
     static {
+        propsToChange = new HashMap<>();
+        propsToChangePixel6 = new HashMap<>();
+        propsToChangePixel6.put("BRAND", "google");
+        propsToChangePixel6.put("MANUFACTURER", "Google");
+        propsToChangePixel6.put("DEVICE", "raven");
+        propsToChangePixel6.put("PRODUCT", "raven");
+        propsToChangePixel6.put("MODEL", "Pixel 6 Pro");
+        propsToChangePixel6.put(
+                "FINGERPRINT", "google/raven/raven:12/SQ3A.220705.003/8671607:user/release-keys");
         propsToKeep = new HashMap<>();
         propsToKeep.put("com.google.android.settings.intelligence", new ArrayList<String>(Arrays.asList("FINGERPRINT")));
         propsToKeep.put("com.google.android.gms", new ArrayList<String>(Arrays.asList("MODEL")));
-        propsToChange = new HashMap<>();
         propsToChange.put("BRAND", "google");
         propsToChange.put("MANUFACTURER", "Google");
         propsToChange.put("DEVICE", "redfin");
@@ -89,7 +99,7 @@ public class PixelPropsUtils {
         }
 
         if (Arrays.asList(pixelCodenames).contains(SystemProperties.get(DEVICE))) {
-            if ((packageName.equals(PACKAGE_GMS) || packageName.equals(PACKAGE_ANDROID) || packageName.equals(PACKAGE_LATIN))  && app.getProcessName().equals("com.google.android.gms.unstable")) {
+            if (packageName.equals(PACKAGE_GMS) && app.getProcessName().equals("com.google.android.gms.unstable")) {
                 setPropValue("MODEL", SystemProperties.get("ro.product.model") + " ");
                 setPropValue("PRODUCT", SystemProperties.get(DEVICE));
                 sIsGms = true;
@@ -114,10 +124,21 @@ public class PixelPropsUtils {
                 setPropValue(key, value);
             }
         }
+        if (contains(packageName, packagesToChangePixel6)) {
+            propsToChange.putAll(propsToChangePixel6);
+        }
         // Set proper indexing fingerprint
         if (packageName.equals("com.google.android.settings.intelligence")){
             setPropValue("FINGERPRINT", Build.DATE);
         }
+    }
+
+    private static boolean contains(String value, String[] array) {
+        for (String i : array) {
+            if (value.contains(i))
+                return true;
+        }
+        return false;
     }
 
     private static void setPropValue(String key, Object value){
