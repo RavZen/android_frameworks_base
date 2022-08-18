05078a33d5064b095bf35f6a23bca299908f66f7
Viktor Hermann <v.hermann.it@gmail.com>
2022-08-19 00:30:17 +0200

    test

--- packages/SystemUI/src/com/android/keyguard/clock/ClockLayout.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockLayout.java
@@ -73,7 +73,7 @@ public class ClockLayout extends FrameLayout {
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        super.onLayout(changed, left, top, right, bottom);
+        super.onLayout(changed, left, top, right, bottom + 40);
         positionChildren();
     }
 
